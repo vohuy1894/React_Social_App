61e0f3acaded63d235dfd8f4b45a95c8c313533c
Huy Vo <vohuy1894@gmail.com>
2022-07-12 14:33:08 -0500

    Add video card component

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,15 +2,26 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import axios from "axios";
+import {Video} from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from "../components/NoResults";
 
-// interface IProps{ 
-//   videos: 
-// }
+interface IProps{ 
+  videos: Video[]
+}
 
-const Home: NextPage = ({videos}) => {
+const Home = ({videos} : IProps) => {
   console.log(videos);
   return (
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div className="flex flex-col gap-10 videos h-full">
+    {videos.length ? (videos.map((video: Video) => (
+      <VideoCard post={video} key={video._id}/>
+    )))
+      : ( 
+        <NoResults text={'No Videos'}/>
+      )
+    }
+  </div>
   )
 };
 
