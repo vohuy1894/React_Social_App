87455ba2d7420dc49385544152581088988e8521
Huy Vo <vohuy1894@gmail.com>
2022-07-14 14:23:31 -0500

    Add topic api and search function

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,37 +1,41 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import axios from "axios";
-import {Video} from '../types';
+import React from 'react';
+import axios from 'axios';
+
 import VideoCard from '../components/VideoCard';
-import NoResults from "../components/NoResults";
-import {BASE_URL} from '../utils';
-interface IProps{ 
-  videos: Video[]
+import { BASE_URL } from '../utils';
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
+
+interface IProps {
+  videos: Video[];
 }
 
-const Home = ({videos} : IProps) => {
-  console.log(videos);
+const Home = ({ videos }: IProps) => {
   return (
-  <div className="flex flex-col gap-10 videos h-full">
-    {videos.length ? (videos.map((video: Video) => (
-      <VideoCard post={video} key={video._id}/>
-    )))
-      : ( 
-        <NoResults text={'No Videos'}/>
-      )
-    }
-  </div>
-  )
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length 
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video}  key={video._id} />
+        )) 
+        : <NoResults text={`No Videos`} />}
+    </div>
+  );
 };
 
-export const getServerSideProps = async () => {
-  const {data} = await axios.get(`${BASE_URL}/api/post`);
-  return {
-    props: {
-      videos: data
-    }
-  }
-}
-
 export default Home;
+
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = await axios.get(`${BASE_URL}/api/post`);
+
+  if(topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  }
+  
+  return {
+    props: { videos: response.data },
+  };
+};
